0990932f144a868c7f1ebbc52c1d4a0e33122fd4
Sonja Goerlich <null>
2021-05-23 18:33:58 +0200

    Added Unit test to price API

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingControllerTest.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingControllerTest.java
@@ -12,14 +12,23 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.net.URI;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-@RunWith(SpringRunner.class)
+/*@RunWith(SpringRunner.class)
+@SpringBootTest
 @AutoConfigureMockMvc
-@WebMvcTest(PricingController.class)
+@WebMvcTest(PricingController.class)*/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 public class PricingControllerTest {
 
     @Autowired
@@ -28,6 +37,13 @@ public class PricingControllerTest {
     @MockBean
     private PricingService pricingService;
 
+    @Test
+    public void getPrice() throws Exception {
+        mockMvc.perform(
+                get("services/price/{id}", 1)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+        ).andExpect(status().isNotFound());
+    }
 
 
 
--- P02-VehiclesAPI/vehicles-api/src/test/java/com/udacity/vehicles/VehiclesApiApplicationTests.java
+++ P02-VehiclesAPI/vehicles-api/src/test/java/com/udacity/vehicles/VehiclesApiApplicationTests.java
@@ -13,4 +13,6 @@ public class VehiclesApiApplicationTests {
     public void contextLoads() {
     }
 
+
+
 }
